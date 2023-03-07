aab812fcaa17a3da455d52b1adbb1b5636ce1735
greedzZz <anvariphone2016@gmail.com>
2023-03-07 18:04:11 +0300

    added methods using the name field in NoteRepository

--- src/main/java/greedzzz/notetakingapp/repositories/NoteRepository.java
+++ src/main/java/greedzzz/notetakingapp/repositories/NoteRepository.java
@@ -4,4 +4,7 @@ import greedzzz.notetakingapp.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Integer> {
+    boolean existsByName(String name);
+    void deleteByName(String name);
+    Note findByName(String name);
 }
