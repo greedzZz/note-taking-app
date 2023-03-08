b9a110d8c9f37ec54ba3b4aedae70e51fcbe5af2
greedzZz <anvariphone2016@gmail.com>
2023-03-08 19:22:47 +0300

    Main class fix

--- src/main/java/greedzzz/notetakingapp/NoteTakingAppApplication.java
+++ src/main/java/greedzzz/notetakingapp/NoteTakingAppApplication.java
@@ -3,19 +3,10 @@ package greedzzz.notetakingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 @SpringBootApplication
 public class NoteTakingAppApplication {
 
     public static void main(String[] args) {
-        List<String> argList = new ArrayList<>(Arrays.asList(args));
-        argList.add("--spring.shell.command.history.enabled=false");
-        argList.add("--spring.shell.command.script.enabled=false");
-        argList.add("--spring.shell.command.stacktrace.enabled=false");
-        args = argList.toArray(new String[0]);
         SpringApplication.run(NoteTakingAppApplication.class, args);
     }
 
