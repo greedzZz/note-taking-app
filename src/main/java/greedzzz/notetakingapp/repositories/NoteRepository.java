2084f8ff9dbcde66babf3ded4e1c9e9013b9d271
greedzZz <anvariphone2016@gmail.com>
2023-03-08 14:31:05 +0300

    findByName method returns Optional

--- src/main/java/greedzzz/notetakingapp/repositories/NoteRepository.java
+++ src/main/java/greedzzz/notetakingapp/repositories/NoteRepository.java
@@ -3,8 +3,10 @@ package greedzzz.notetakingapp.repositories;
 import greedzzz.notetakingapp.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NoteRepository extends JpaRepository<Note, Integer> {
     boolean existsByName(String name);
     void deleteByName(String name);
-    Note findByName(String name);
+    Optional<Note> findByName(String name);
 }
